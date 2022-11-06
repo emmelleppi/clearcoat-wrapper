502626095edadfad58fc5829f0cac2ed0923f43c
Marco Ludovico Perego <mlp.perego@gmail.com>
2022-11-06 16:38:22 +0100

    adds bloom and lut

--- src/usePostprocessing.js
+++ src/usePostprocessing.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import * as THREE from "three";
 import {
@@ -6,6 +6,12 @@ import {
   KernelSize,
   MipmapBlurPass,
   KawaseBlurPass,
+  EffectPass,
+  BloomEffect,
+  BlendMode,
+  BlendFunction,
+  LUT3DEffect,
+  LookupTexture3D,
 } from "postprocessing";
 import customVert from "./customPass.vert"
 import customFrag from "./customPass.frag"
@@ -30,13 +36,25 @@ const myShaderMaterial = new THREE.ShaderMaterial({
 function usePostprocessing({ lut, envDiffuse, envSpecular }) {
   const { gl, scene, camera, size } = useThree();
 
-  const [outputNormBuffer, outputPosBuffer] = useState(() => {
+  const lutTexture = useTexture("/lut/filmic2.png")
+  const distortionTexture = useTexture("/distortion.jpg")
+  distortionTexture.wrapS = distortionTexture.wrapT = THREE.RepeatWrapping
+
+  const buffers = useState(() => {
     const rt = new THREE.WebGLRenderTarget(
       window.innerWidth * window.devicePixelRatio,
       window.innerHeight * window.devicePixelRatio,
+      {
+        minFilter: THREE.LinearFilter,
+        magFilter: THREE.LinearFilter,
+        stencilBuffer: false,
+        depthBuffer:  false,
+        type: THREE.HalfFloatType
+      }
     )
-    return [rt.clone(), rt.clone()]
+    return [rt.clone(), rt.clone(), rt.clone()]
   })[0]
+  const [outputNormBuffer, outputPosBuffer, buffer] = buffers
 
   const rt = useState(() => {
     const renderTarget = new THREE.WebGLMultipleRenderTargets(
@@ -48,12 +66,12 @@ function usePostprocessing({ lut, envDiffuse, envSpecular }) {
     renderTarget.texture[ 0 ].minFilter = THREE.LinearMipmapLinearFilter;
     renderTarget.texture[ 0 ].magFilter = THREE.LinearFilter;
     renderTarget.texture[ 0 ].generateMipmaps = true
-
+    
     renderTarget.texture[ 1 ].minFilter = THREE.NearestFilter;
     renderTarget.texture[ 1 ].magFilter = THREE.NearestFilter;
     renderTarget.texture[ 2 ].minFilter = THREE.NearestFilter;
     renderTarget.texture[ 2 ].magFilter = THREE.NearestFilter;
-
+    
     renderTarget.texture[ 0 ].name = 'diffuse';
     renderTarget.texture[ 1 ].name = 'normal';
     renderTarget.texture[ 2 ].name = 'position';
@@ -63,7 +81,7 @@ function usePostprocessing({ lut, envDiffuse, envSpecular }) {
     return renderTarget
   })[0];
 
-  const [customPass, blurPass, mmBlurPass] = useMemo(() => {
+  const pipeline = useMemo(() => {
     const CUSTOM = new ShaderPass(myShaderMaterial, 'u_diffuse')
     
     const MMBLUR = new MipmapBlurPass()
@@ -72,49 +90,40 @@ function usePostprocessing({ lut, envDiffuse, envSpecular }) {
     const BLUR = new KawaseBlurPass()
     BLUR.scale = 3
     BLUR.kernelSize = KernelSize.VERY_LARGE
+
+    const BLOOM = new BloomEffect()
+    BLOOM.blendMode = new BlendMode(BlendFunction.COLOR_DODGE)
+    BLOOM.resolution.height = 360;
+    BLOOM.blurPass.kernelSize = 1;
+    BLOOM.blurPass.scale = 1;
+    BLOOM.intensity = 0.8
+    BLOOM.luminanceMaterial.threshold = 0.3;
+    BLOOM.luminanceMaterial.smoothing = 0.1;
+    BLOOM.inputColorSpace = THREE.LinearEncoding
+    BLOOM.outputColorSpace = THREE.sRGBEncoding
     
-    return [CUSTOM, BLUR, MMBLUR];
+    const lut = LookupTexture3D.from(lutTexture);
+		const LUT = new LUT3DEffect(lut)
+
+    const EFX = new EffectPass(camera, BLOOM, LUT)
+    EFX.initialize(gl, 0, THREE.HalfFloatType);
+
+    return [CUSTOM, BLUR, MMBLUR, EFX];
   }, [gl, scene, camera, rt]);
 
+  const [customPass, blurPass, mmBlurPass, efxPass] = pipeline
+
   useEffect(() => {
-    customPass.setSize(size.width, size.height)
-    blurPass.setSize(size.width, size.height)
-    outputNormBuffer.setSize(size.width, size.height)
-    outputPosBuffer.setSize(size.width, size.height)
-    mmBlurPass.setSize(size.width, size.height)
+    pipeline.forEach(el => el.setSize(size.width, size.height));
+    buffers.forEach(el => el.setSize(size.width, size.height));
+    rt.setSize(size.width, size.height)
   }, [
     size.width,
     size.height,
-    customPass,
-    blurPass,
-    outputNormBuffer,
-    outputPosBuffer,
-    mmBlurPass,
+    pipeline,
+    rt,
+    buffers,
   ])
-
-  const distortionTexture = useTexture("/distortion.jpg")
-  distortionTexture.wrapS = distortionTexture.wrapT = THREE.RepeatWrapping
-
-  const blur = useCallback((texture, outputBuffer) => {
-    const kernelSequence = blurPass.blurMaterial.kernelSequence;
-
-    let previousTexture = texture
-		blurPass.fullscreenMaterial = blurPass.blurMaterial;
- 
-		for(let i = 0, l = kernelSequence.length; i < l; ++i) {
-			const buffer = ((i & 1) === 0) ? blurPass.renderTargetA : blurPass.renderTargetB;
-			blurPass.blurMaterial.kernel = kernelSequence[i];
-			blurPass.blurMaterial.inputBuffer = previousTexture;
-			gl.setRenderTarget(buffer);
-			gl.render(blurPass.scene, blurPass.camera);
-			previousTexture = buffer.texture;
-		}
- 
-		blurPass.fullscreenMaterial = blurPass.copyMaterial;
-		blurPass.copyMaterial.inputBuffer = previousTexture;
-		gl.setRenderTarget(outputBuffer);
-		gl.render(blurPass.scene, blurPass.camera);
-  }, [blurPass, gl])
 
   useFrame((_, delta) => {
     gl.setRenderTarget( rt );
@@ -125,11 +134,15 @@ function usePostprocessing({ lut, envDiffuse, envSpecular }) {
       width: rt.width,
       height: rt.height
     }, null, delta, false)
-    
     rt.texture[ 0 ].mipmaps = mmBlurPass.downsamplingMipmaps
+
+    blurPass.render(gl, {
+      texture: rt.texture[ 1 ],
+    }, outputNormBuffer, delta, false)
     
-    blur(rt.texture[ 1 ], outputNormBuffer)
-    blur(rt.texture[ 2 ], outputPosBuffer)
+    blurPass.render(gl, {
+      texture: rt.texture[ 2 ],
+    }, outputPosBuffer, delta, false)
     
     myShaderMaterial.uniforms.u_time.value += delta
     myShaderMaterial.uniforms.u_distortion.value = distortionTexture
@@ -141,7 +154,8 @@ function usePostprocessing({ lut, envDiffuse, envSpecular }) {
     myShaderMaterial.uniforms.u_envDiffuse.value = envDiffuse
     myShaderMaterial.uniforms.u_envSpecular.value = envSpecular
     
-    customPass.render(gl, null, null, delta, false)
+    customPass.render(gl, null, buffer, delta, false)
+    efxPass.render(gl, buffer, null, delta, false)
   }, 1);
 }
 
